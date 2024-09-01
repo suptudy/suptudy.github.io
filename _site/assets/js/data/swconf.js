d625e3c0c4b5c948de8cd7553343c81a266f3476
Subin Kwon <rnjstnqls99@naver.com>
2024-09-01 14:36:27 +0900

    0901_suptudy_info

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1725167412',resources: [
+    cacheName: 'chirpy-1725168964',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
